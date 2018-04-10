7555881e6848f592e74da711e077c26d0b931eac
zhikangD <zhikang@photobutler.com>
2018-04-10 15:14:23 -0400

    .

--- donkey.py
+++ donkey.py
@@ -34,10 +34,12 @@ class Donkey(object):
         filename_queue = tf.train.string_input_producer([path_to_tfrecords_file], num_epochs=None)
         image, length, digits = Donkey._read_and_decode(filename_queue)
 
-        min_queue_examples = int(0.4 * num_examples)
+        # min_queue_examples = int(0.4 * num_examples)
+        min_queue_examples = 32
         print('min_queue_examples  ', min_queue_examples)
         if shuffled:
             image_batch, length_batch, digits_batch = tf.train.shuffle_batch([image, length, digits],
+                                                                             enqueue_many=True,
                                                                              batch_size=batch_size,
                                                                              num_threads=2,
                                                                              capacity=min_queue_examples + 3 * batch_size,
